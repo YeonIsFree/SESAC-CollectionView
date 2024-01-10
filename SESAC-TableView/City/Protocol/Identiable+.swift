2cd79b0d3e04824cfcba101e4400b0f17251d8ce
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2024-01-11 02:25:41 +0900

    SESAC 12회차 과제 : Refactor

---
+++ SESAC-TableView/City/Protocol/Identiable+.swift
@@ -0,0 +1,8 @@
+//
+//  Identiable+.swift
+//  SESAC-TableView
+//
+//  Created by Seryun Chun on 2024/01/11.
+//
+
+import Foundation
---
+++ SESAC-TableView/City/Protocol/Niable+.swift
@@ -0,0 +1,8 @@
+//
+//  Niable+.swift
+//  SESAC-TableView
+//
+//  Created by Seryun Chun on 2024/01/11.
+//
+
+import Foundation
